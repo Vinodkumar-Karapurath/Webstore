13e164295a6f842e75223fd6ff5e873dd7a0b482
vinod <vinodknice@gmail.com>
2024-04-02 22:12:01 +0300

    up1

---
+++ Models/ViewModels/LoginViewModels.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Webstore.Models.ViewModels
+{
+    public class LoginViewModels
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Username is required")]
+        public string? Username { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password is required")]
+        public string? Password { get; set; }
+    }
+}
