08b6e5c6637e997f791f43972b94299599fdb56b
vinodkumar <k.vinod@mirnah.com>
2024-04-03 15:23:38 +0300

    421

--- Models/ViewModels/LoginViewModels.cs
+++ Models/ViewModels/LoginViewModels.cs
@@ -4,9 +4,10 @@ namespace Webstore.Models.ViewModels
 {
     public class LoginViewModels
     {
-        [Required(AllowEmptyStrings = false, ErrorMessage = "Username is required")]
+        [Required(ErrorMessage = "Username is required")]
         public string? Username { get; set; }
-        [Required(AllowEmptyStrings = false, ErrorMessage = "Password is required")]
+
+        [Required(ErrorMessage = "Password is required")]
         public string? Password { get; set; }
     }
 }
